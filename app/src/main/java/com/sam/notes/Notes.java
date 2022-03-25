7107c36da2cfafd7e1dd4280d800ecb4f294debc
jeSuisCodeuse <creativegirl@outlook.fr>
2022-03-25 14:56:00 +0100

    Realm doesnt wanna instal !

--- app/src/main/java/com/sam/notes/Notes.java
+++ app/src/main/java/com/sam/notes/Notes.java
@@ -1,4 +1,41 @@
 package com.sam.notes;
 
+import android.app.Application;
+
+import java.util.Date;
+
+
+// I CANNOT INSTALL REALM  why the hell is that? I've tried everything
+
+
 public class Notes {
+
+
+    String title;
+    String text;
+    Date createdAt;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
 }
