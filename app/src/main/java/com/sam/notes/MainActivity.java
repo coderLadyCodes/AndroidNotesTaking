97baf665910b1ae8a4fe15f1893a8c8d0a765776
jeSuisCodeuse <creativegirl@outlook.fr>
2022-03-26 17:31:22 +0100

    add

--- app/src/main/java/com/sam/notes/MainActivity.java
+++ app/src/main/java/com/sam/notes/MainActivity.java
@@ -3,6 +3,9 @@ package com.sam.notes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 import io.realm.Realm;
 
@@ -14,6 +17,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Realm.init(this);
+       // Realm.init(this);
+
+        MaterialButton addNoteBtn =  findViewById(R.id.addnewnotebtn);
+        addNoteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
