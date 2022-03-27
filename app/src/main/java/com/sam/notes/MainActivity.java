c1c013801c58b0b1a0b4fbdc176758c68d44a1a7
jeSuisCodeuse <creativegirl@outlook.fr>
2022-03-27 18:11:16 +0200

    add drawable

--- app/src/main/java/com/sam/notes/MainActivity.java
+++ app/src/main/java/com/sam/notes/MainActivity.java
@@ -2,6 +2,7 @@ package com.sam.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -23,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         addNoteBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+            startActivity(new Intent(MainActivity.this,AddNoteActivity.class));
             }
         });
     }
