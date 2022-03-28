dd696ad09383b02dd5b14c3f4fb58073f89b52d9
jeSuisCodeuse <creativegirl@outlook.fr>
2022-03-28 20:36:37 +0200

    end

--- app/src/main/java/com/sam/notes/MainActivity.java
+++ app/src/main/java/com/sam/notes/MainActivity.java
@@ -1,6 +1,8 @@
 package com.sam.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,6 +11,8 @@ import android.view.View;
 import com.google.android.material.button.MaterialButton;
 
 import io.realm.Realm;
+import io.realm.RealmChangeListener;
+import io.realm.RealmResults;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -18,13 +22,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       // Realm.init(this);
+
 
         MaterialButton addNoteBtn =  findViewById(R.id.addnewnotebtn);
         addNoteBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
             startActivity(new Intent(MainActivity.this,AddNoteActivity.class));
+            }
+        });
+
+        Realm.init(getApplicationContext());
+        Realm realm = Realm.getDefaultInstance();
+
+        RealmResults<Notes> notesList = realm.where(Notes.class).findAll();
+
+        RecyclerView recyclerView = findViewById(R.id.recycleview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        MyAdapter myAdapter = new MyAdapter(getApplicationContext(), notesList);
+        recyclerView.setAdapter(myAdapter);
+
+        notesList.addChangeListener(new RealmChangeListener<RealmResults<Notes>>() {
+            @Override
+            public void onChange(RealmResults<Notes> notes) {
+                myAdapter.notifyDataSetChanged();
             }
         });
     }
--- app/src/main/java/com/sam/notes/MyAdapter.java
+++ app/src/main/java/com/sam/notes/MyAdapter.java
@@ -1,13 +1,21 @@
 package com.sam.notes;
 
 import android.content.Context;
+import android.provider.ContactsContract;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.text.DateFormat;
+
+import io.realm.Realm;
 import io.realm.RealmResults;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
@@ -23,17 +31,47 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        return new MyViewHolder(LayoutInflater.from(context).inflate(R.layout.item_view,parent,false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Notes note = notesList.get(position);
+        holder.titleOutput.setText(note.getTitle());
+        holder.textOutput.setText(note.getText());
+
+        String formatedTime = DateFormat.getTimeInstance().format(note.createdAt);
+        holder.timeOutput.setText(formatedTime);
+
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                PopupMenu menu = new PopupMenu(context,v);
+                menu.getMenu().add("DELETE");
+                menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        if(item.getTitle().equals("DELETE")){
+                            Realm realm = Realm.getDefaultInstance();
+                            realm.beginTransaction();
+                            note.deleteFromRealm();
+                            realm.commitTransaction();
+                            Toast.makeText(context, "Note Deleted", Toast.LENGTH_SHORT).show();
+                        }
+                        return true;
+                    }
+                });
+                menu.show();
+
+                return true;
+            }
+        });
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return notesList.size();
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
@@ -50,4 +88,6 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
         }
     }
+
+
 }
